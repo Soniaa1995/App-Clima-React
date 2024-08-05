db2cb71528f03366103b23ac337bcae8c4c7eb5d
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-05 11:36:13 +0200

    ocultando la key de la api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -3,7 +3,7 @@ import { SearchType } from "../types";
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
-    const appId = "f6b6a19b45d94bfa321e0f8510bc9ebb";
+    const appId = import.meta.env.VITE_API_KEY;
 
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
