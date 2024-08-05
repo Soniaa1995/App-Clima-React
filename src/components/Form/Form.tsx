cf7325dac839969732930ea0596dd8364b87a539
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-05 10:12:54 +0200

    implementando useState para ciudad y pais

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,45 @@
 import { countries } from "../../data/countries";
-import styles from './Form.module.css'
+import styles from "./Form.module.css";
+import { useState, ChangeEvent } from "react";
+import type { SearchType } from "../../types";
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form className={styles.form}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad</label>
-        <input id="city" type="text" name="city" placeholder="Ciudad" />
+        <input
+          id="city"
+          type="text"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.field}>
-        <label htmlFor="city">Pais</label>
-        <select>
+        <label htmlFor="country">Pais</label>
+        <select
+          id="country"
+          value={search.country}
+          name="country"
+          onChange={handleChange}
+        >
           <option value="">--Seleccione un país--</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
